b1abee693f8661fd5c6c485887bb6b9076c1006c
Jackie Cai <z5259449@ad.unsw.edu.au>
2022-03-11 00:22:22 +1100

    doing cafe need to fix wait to order

--- kern/asst1/cafe.c
+++ kern/asst1/cafe.c
@@ -10,6 +10,16 @@ static unsigned int ticket_counter; /* the next customer's ticket */
 static unsigned int next_serving;   /* the barista's next ticket to serve */
 static unsigned current_customers;  /* customers remaining in cafe */
 
+struct cv *cv_cust[NUM_CUSTOMERS];
+struct cv *cv_bar[NUM_BARISTAS];
+
+unsigned long customer[NUM_CUSTOMERS];
+unsigned long barista[NUM_BARISTAS];
+
+struct lock *ticket_lock;
+struct lock *serve_lock;
+struct lock *customer_lock;
+struct lock *table_lock;
 
 /*
  * get_ticket: generates an ticket number for a customers next order.
@@ -18,9 +28,10 @@ static unsigned current_customers;  /* customers remaining in cafe */
 unsigned int get_ticket(void)
 {
         unsigned int t;
-
+        lock_acquire(ticket_lock);
         ticket_counter = ticket_counter + 1;
         t = ticket_counter;
+        lock_release(ticket_lock);
 
         return t;
 }
@@ -34,9 +45,10 @@ unsigned int next_ticket_to_serve(void)
 {
 
         unsigned int t;
-
+        lock_acquire(serve_lock);
         next_serving = next_serving + 1;
         t = next_serving;
+        lock_release(serve_lock);
 
         return t;
 }
@@ -49,8 +61,9 @@ unsigned int next_ticket_to_serve(void)
 void leave_cafe(unsigned long customer_num)
 {
         (void)customer_num;
-
+        lock_acquire(customer_lock);
         current_customers = current_customers - 1;
+        lock_release(customer_lock);
 
 }
 
@@ -59,7 +72,7 @@ void leave_cafe(unsigned long customer_num)
  * wait_to_order() and announce_serving_ticket() work together to
  * achieve the following:
  *
- * A customer thread calling wait_to_order will block on a synch primitive
+ * A customer thread calP(empty);ling wait_to_order will block on a synch primitive
  * until announce_serving_ticket is called with the matching ticket.
  *
  * A barista thread calling announce_serving_ticket will block on a synch
@@ -75,21 +88,42 @@ void leave_cafe(unsigned long customer_num)
 
 unsigned long wait_to_order(unsigned long customer_number, unsigned int ticket)
 {
-        unsigned long barista_number = 0;
+        unsigned long barista_number = 1000;
 
-        (void) customer_number;
-        (void) ticket;
-      
+        lock_acquire(table_lock);
+        customer[customer_number] = ticket;
+        for(int i = 0; i <= NUM_BARISTAS;i++){
+            if(i == NUM_BARISTAS){
+                cv_wait(cv_cust[customer_number], table_lock);
+            }
+
+            else if(barista[i] == ticket){
+                barista_number = i;
+                cv_signal(cv_bar[i], table_lock);
+                break;
+            }
+        }
+        lock_release(table_lock);
         return barista_number;
 }
 
 unsigned long announce_serving_ticket(unsigned long barista_number, unsigned int serving)
 {
-        unsigned long cust = 0;
+        unsigned long cust = 1000;
 
-        (void) barista_number;
-        (void) serving;
-
+        lock_acquire(table_lock);
+        barista[barista_number] = serving;
+        for(int i = 0; i <= NUM_CUSTOMERS;i++){
+            if(i == NUM_CUSTOMERS){
+                cv_wait(cv_bar[barista_number], table_lock);
+            }
+            else if(customer[i] == serving){
+                cust = i;
+                cv_signal(cv_cust[i], table_lock);
+                break;
+            }
+        }
+        lock_release(table_lock);
         return cust;
 }
 
@@ -105,6 +139,30 @@ void cafe_startup(void)
         next_serving = 0;
         current_customers = NUM_CUSTOMERS;
 
+        ticket_lock = lock_create("ticket lock");
+        if(ticket_lock == NULL){
+            panic("ticket lock failed to allocate");
+        }
+        table_lock = lock_create("table lock");
+        if(table_lock == NULL){
+            panic("table lock failed to allocate");
+        }
+        serve_lock = lock_create("serve lock");
+        if(serve_lock == NULL){
+            panic("serve lock failed to allocate");
+        }
+        customer_lock = lock_create("customer lock");
+        if(customer_lock == NULL){
+            panic("customer lock failed to allocate");
+        }
+
+        for(int i = 0; i < NUM_CUSTOMERS; i++){
+            cv_cust[i] = cv_create("customer");
+        }
+        for(int i = 0; i < NUM_BARISTAS; i++){
+            cv_bar[i] = cv_create("barista");
+        }
+
 }   
 
 /*
@@ -116,5 +174,15 @@ void cafe_startup(void)
 
 void cafe_shutdown(void)
 {
+        lock_destroy(ticket_lock);
+        lock_destroy(serve_lock);
+        lock_destroy(customer_lock);
+        lock_destroy(table_lock);
+        for(int i = 0; i < NUM_CUSTOMERS; i++){
+            cv_destroy(cv_cust[i]);
+        }
+        for(int i = 0; i < NUM_BARISTAS; i++){
+            cv_destroy(cv_bar[i]);
+        }
 }
                               

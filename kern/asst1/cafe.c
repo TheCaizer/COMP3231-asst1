7390e7670ec3faf2021bf5eec495d5c641fb2754
Jackie Cai <z5259449@ad.unsw.edu.au>
2022-03-11 01:27:55 +1100

    finished cafe and added comments for code

--- kern/asst1/cafe.c
+++ kern/asst1/cafe.c
@@ -9,13 +9,13 @@
 static unsigned int ticket_counter; /* the next customer's ticket */
 static unsigned int next_serving;   /* the barista's next ticket to serve */
 static unsigned current_customers;  /* customers remaining in cafe */
-
+// the cv for all customers and barista
 struct cv *cv_cust[NUM_CUSTOMERS];
 struct cv *cv_bar[NUM_BARISTAS];
-
+// array to keep track of the ticket number of customers and barista
 unsigned long customer[NUM_CUSTOMERS];
 unsigned long barista[NUM_BARISTAS];
-
+// all the locks
 struct lock *ticket_lock;
 struct lock *serve_lock;
 struct lock *customer_lock;
@@ -28,6 +28,7 @@ struct lock *table_lock;
 unsigned int get_ticket(void)
 {
         unsigned int t;
+        // lock the ticket
         lock_acquire(ticket_lock);
         ticket_counter = ticket_counter + 1;
         t = ticket_counter;
@@ -43,8 +44,8 @@ unsigned int get_ticket(void)
 
 unsigned int next_ticket_to_serve(void)
 {
-
         unsigned int t;
+        // lock the serving ticket
         lock_acquire(serve_lock);
         next_serving = next_serving + 1;
         t = next_serving;
@@ -61,6 +62,7 @@ unsigned int next_ticket_to_serve(void)
 void leave_cafe(unsigned long customer_num)
 {
         (void)customer_num;
+        // lock the customer decrement
         lock_acquire(customer_lock);
         current_customers = current_customers - 1;
         lock_release(customer_lock);
@@ -88,23 +90,28 @@ void leave_cafe(unsigned long customer_num)
 
 unsigned long wait_to_order(unsigned long customer_number, unsigned int ticket)
 {
+        // the barista_number, big number for debugging purpose
         unsigned long barista_number = 200000000;
-
+        // lock the customer and barista
         lock_acquire(table_lock);
+        // set the assign ticket to customer
         customer[customer_number] = ticket;
+        // look for barista with that ticket
         for(int i = 0; i <= NUM_BARISTAS;i++){
+            // no barista with ticket therefore we wait
             if(i == NUM_BARISTAS){
                 cv_wait(cv_cust[customer_number], table_lock);
-                // change this to find the number that the barista is
+                // someone signalled therefore we find the barista number
                 for(int l = 0; l < NUM_BARISTAS;l++){
                     if(barista[l] == ticket){
                         barista_number = l;
                     }
                 }
             }
-
+            // we found a barista with the ticket
             else if(barista[i] == ticket){
                 barista_number = i;
+                // signal the barista that we have the ticket if waiting
                 cv_signal(cv_bar[i], table_lock);
                 break;
             }
@@ -115,22 +122,28 @@ unsigned long wait_to_order(unsigned long customer_number, unsigned int ticket)
 
 unsigned long announce_serving_ticket(unsigned long barista_number, unsigned int serving)
 {
+        // the customer number, big number for debugging purpose
         unsigned long cust = 100000000;
 
         lock_acquire(table_lock);
+        // assign serving number to barista
         barista[barista_number] = serving;
+        // find customer with the number
         for(int j = 0; j <= NUM_CUSTOMERS;j++){
+            // no customer with the number therefore we wait
             if(j == NUM_CUSTOMERS){
                 cv_wait(cv_bar[barista_number], table_lock);
-                // change this to find the number that the barista is
+                // someone signalled the number we now find the customer
                 for(int k = 0; k < NUM_CUSTOMERS;k++){
                     if(customer[k] == serving){
                         cust = k;
                     }
                 }
             }
+            // we found a customer with the ticket
             else if(customer[j] == serving){
                 cust = j;
+                // signal the customer
                 cv_signal(cv_cust[j], table_lock);
                 break;
             }
@@ -150,7 +163,7 @@ void cafe_startup(void)
         ticket_counter = 0;
         next_serving = 0;
         current_customers = NUM_CUSTOMERS;
-
+        // Make all the locks and then panic if cant allocate any
         ticket_lock = lock_create("ticket lock");
         if(ticket_lock == NULL){
             panic("ticket lock failed to allocate");
@@ -167,7 +180,7 @@ void cafe_startup(void)
         if(customer_lock == NULL){
             panic("customer lock failed to allocate");
         }
-
+        // create a cv for all the customer and barista
         for(int i = 0; i < NUM_CUSTOMERS; i++){
             cv_cust[i] = cv_create("customer");
         }
@@ -186,6 +199,7 @@ void cafe_startup(void)
 
 void cafe_shutdown(void)
 {
+        // destroy all lock and cv, no free function for array
         lock_destroy(ticket_lock);
         lock_destroy(serve_lock);
         lock_destroy(customer_lock);
